40a08989cc5e94df88edce0e9dc2b2d02065db5e
Fernando de Moraes Sedrez <fmsedrez@gmail.com>
2021-07-01 10:34:24 -0300

    Feature: First DAG.
    This is a hourly DAG that runs a bash command 'date' and show the output
    in the DAG's log.

---
+++ dags/first_dag.py
@@ -0,0 +1,29 @@
+"""First DAG Module."""
+
+from datetime import timedelta
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    "first_dag",
+    description="Simple first DAG.",
+    schedule_interval="@hourly",
+    start_date=days_ago(1),
+    catchup=False,
+    tags=["example", "simple"],
+) as dag:
+    task_one = BashOperator(
+        task_id="show_time",
+        bash_command="date",
+    )
